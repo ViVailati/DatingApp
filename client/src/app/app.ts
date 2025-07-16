f45f686537e29225d94a26a3a9cbca10f0b91cf3
Vi Vailati <vi.vailati.845@gmail.com>
2025-07-15 23:26:26 -0600

    display initial data from api

--- client/src/app/app.ts
+++ client/src/app/app.ts
@@ -1,12 +1,29 @@
-import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import {Component, inject, OnInit, signal} from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {NgOptimizedImage} from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [
+    NgOptimizedImage
+  ],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
-export class App {
-  protected readonly title = signal('client');
+export class App implements OnInit {
+  private http = inject(HttpClient);
+
+  protected readonly title = signal('Dating app');
+  protected members = signal<any>([]);
+
+  ngOnInit(): void {
+    this.http.get("https://localhost:7087/api/members").subscribe({
+      next: res => {
+        this.members.set(res);
+      },
+      error: err => {
+        console.error(err);
+      }
+    });
+  }
 }
